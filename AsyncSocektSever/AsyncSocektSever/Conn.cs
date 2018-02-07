f225396022f13528c047838919ce5d3f512215d6
许 <13421491617@163.com>
2018-02-07 16:16:30 +0800

    create class Conn and Serv
    AsyncAceept and Receive Message

---
+++ AsyncSocektSever/AsyncSocektSever/Conn.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AsyncSocektSever
+{
+    class Conn
+    {
+        public const int BUFFER_SIZE = 1024;
+        public Socket socket;
+        public bool isUse = false;
+        //缓冲区
+        public byte[] readbuffer = null;
+        public int buffCount = 0;
+        public Conn()
+        {
+            readbuffer = new byte[BUFFER_SIZE];
+        }
+        public void Init(Socket socket)
+        {
+            this.socket = socket;
+            isUse = true;
+            buffCount = 0;
+        }
+        //缓冲区剩余的字节数
+        public int Remain()
+        {
+            return BUFFER_SIZE - buffCount;
+        }
+        public string GetAddress()
+        {
+            if (!isUse)
+            {
+                return "无法获取地址";
+            }
+            return socket.RemoteEndPoint.ToString();
+        }
+        public void Close()
+        {
+            if (!isUse)
+            {
+                return;
+            }
+            Console.WriteLine("断开连接" + GetAddress());
+            socket.Close();
+            isUse = false;
+        }
+    }
+}
---
+++ AsyncSocektSever/AsyncSocektSever/Serv.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Sockets;
+using System.Net;
+
+namespace AsyncSocektSever
+{
+    class Serv
+    {
+        public Socket listenfd;
+        public Conn[] conns;
+        public int maxConn = 50;
+        //获取连接池空余位置的下标
+        public int NewIndex()
+        {
+            if (conns == null)
+            {
+                return -1;
+            }
+            for(int i = 0; i < conns.Length; i++)
+            {
+                if (conns[i] == null)
+                {
+                    conns[i] = new Conn();
+                    return i;
+                }
+                else if (conns[i].isUse == false)
+                {
+                    return i;
+                }
+            }
+            return -1;
+        }
+
+        public void Start(string host,int port)
+        {
+            //初始化
+            conns = new Conn[maxConn];
+            for(int i = 0; i < conns.Length; i++)
+            {
+                conns[i] = new Conn();
+            }
+            listenfd = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            IPAddress ipAdr = IPAddress.Parse(host);
+            IPEndPoint ipEp = new IPEndPoint(ipAdr, port);
+            listenfd.Bind(ipEp);
+            listenfd.Listen(maxConn);
+            listenfd.BeginAccept(AcceptCb,null);
+            Console.WriteLine("服务器已启动...");
+        }
+        private void AcceptCb(IAsyncResult ar)
+        {
+            try
+            {
+                //程序停在这里直到有client连接
+                Socket socket=listenfd.EndAccept(ar);
+                int index = NewIndex();
+                if (index <0)
+                {
+                    socket.Close();
+                    Console.WriteLine("连接池已满...");
+                }
+                else
+                {
+                    Conn conn = conns[index];
+                    conn.Init(socket);
+                    string adr = conn.GetAddress();
+                    Console.WriteLine("客户端连接...["+adr+"],连接池Id:"+index);
+                    conn.socket.BeginReceive(conn.readbuffer, conn.buffCount, conn.Remain(), SocketFlags.None, ReceiveCb, conn);
+                    listenfd.BeginAccept(AcceptCb, null);//实现接收循环
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Accepte失败");
+            }
+        }
+        private void ReceiveCb(IAsyncResult result)
+        {
+
+        }
+    }
+}
